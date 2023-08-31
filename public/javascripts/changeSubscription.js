b34db18330f543fab939effe454bd5aa3b9fb4ba
fabio <github@sauna.re>
2023-08-31 14:57:02 +0200

    Fix: Uncaught expression when no plan is active once loading the page Home

--- public/javascripts/changeSubscription.js
+++ public/javascripts/changeSubscription.js
@@ -1,10 +1,14 @@
-document.getElementById('change-subscription-form').addEventListener('submit', function() {
-    var currentPlanId = '#{currentPlan}';
-    var newPlanId;
-    if (currentPlanId == process.env.SUBSCRIPTION_PRICE_ID_MEMBER_6MONTHS) {
-        newPlanId = process.env.SUBSCRIPTION_PRICE_ID_ALUMNI_6MONTHS;
-    } else {
-        newPlanId = process.env.SUBSCRIPTION_PRICE_ID_MEMBER_6MONTHS;
-    }
-    document.getElementById('newPlanId').value = newPlanId;
-});
+var changeSubscriptionForm = document.getElementById('change-subscription-form');
+
+if (changeSubscriptionForm) {
+    changeSubscriptionForm.addEventListener('submit', function() {
+        var currentPlanId = '#{currentPlan}';
+        var newPlanId;
+        if (currentPlanId == process.env.SUBSCRIPTION_PRICE_ID_MEMBER_6MONTHS) {
+            newPlanId = process.env.SUBSCRIPTION_PRICE_ID_ALUMNI_6MONTHS;
+        } else {
+            newPlanId = process.env.SUBSCRIPTION_PRICE_ID_MEMBER_6MONTHS;
+        }
+        document.getElementById('newPlanId').value = newPlanId;
+    });
+}
