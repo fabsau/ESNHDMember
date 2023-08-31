91726f5deb2221904b584f349ce35eda144a2176
fabio <github@sauna.re>
2023-08-31 15:23:40 +0200

    Change title of page on mobile so menu is put correctly

---
+++ public/javascripts/mobileLayout.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    function checkTitle() {
+        if(window.innerWidth <= 576) {
+            document.title = "ESN HD Member Portal";
+            document.querySelector(".navbar-brand span:nth-child(2)").textContent = " ESN HD Member Portal";
+        } else {
+            document.title = "ESN Heidelberg Member Portal";
+            document.querySelector(".navbar-brand span:nth-child(2)").textContent = " ESN Heidelberg Member Portal";
+        }
+    }
+    checkTitle();
+    window.onresize = checkTitle;
+});
