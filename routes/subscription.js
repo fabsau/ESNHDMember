0c465b50fd247195ebd3a1fc6c13a92c8292eed3
fabio <github@sauna.re>
2023-09-03 21:28:14 +0200

    Updated succes/error messages and improved cancellation process. Cancellation can now be undone

--- routes/subscription.js
+++ routes/subscription.js
@@ -41,14 +41,14 @@ module.exports = function (stripe, isAuthenticated) {
       }
       req.session.message = {
         type: "success",
-        text: "Plan was successful changed!",
+        text: "Plan was successful changed! You will receive an email confirmation shortly.",
       };
       res.redirect("/home");
     } catch (error) {
-      console.error("Error upgrading subscription:", error);
+      console.error("Error switching membership type:", error);
       req.session.message = {
         type: "error",
-        text: "Error upgrading subscription, please refresh the page!",
+        text: "Error switching membership type, please refresh the page!",
       };
       res.redirect("/home");
     }
@@ -67,15 +67,17 @@ module.exports = function (stripe, isAuthenticated) {
         const subscriptions = await stripe.subscriptions.list({
           customer: customerId,
         });
-        // Cancel the existing subscription
+        // Cancel the existing subscription at the end of the current period
         if (subscriptions.data.length > 0) {
-          await stripe.subscriptions.cancel(subscriptions.data[0].id);
+          await stripe.subscriptions.update(subscriptions.data[0].id, {
+            cancel_at_period_end: true,
+          });
         }
       }
       // Redirect to the home page with a successful cancellation message
       req.session.message = {
         type: "success",
-        text: "Cancellation was successful!",
+        text: "Cancellation was successful! You will receive an email confirmation shortly.",
       };
       res.redirect("/home");
     } catch (error) {
@@ -89,37 +91,74 @@ module.exports = function (stripe, isAuthenticated) {
     }
   });
 
-  router.post(
-    "/admin-cancel-subscription",
-    isAuthenticated,
-    async (req, res) => {
-      const { email } = req.body;
-
-      try {
-        const customers = await stripe.customers.list({
-          email: email,
-          limit: 1,
+  router.post("/undo-cancel-subscription", isAuthenticated, async (req, res) => {
+    const { email } = req.user;
+    try {
+      // Fetch customer details from Stripe
+      const customer = await stripe.customers.list({
+        email: email,
+        limit: 1,
+      });
+      if (customer.data.length > 0) {
+        const customerId = customer.data[0].id;
+        const subscriptions = await stripe.subscriptions.list({
+          customer: customerId,
         });
+        // Undo subscription cancellation
+        if (subscriptions.data.length > 0) {
+          await stripe.subscriptions.update(subscriptions.data[0].id, {
+            cancel_at_period_end: false,
+          });
+        }
+      }
+      // Redirect to the home page with a successful undo cancellation message
+      req.session.message = {
+        type: "success",
+        text: "Cancellation was successfully undone! You will receive an email confirmation shortly.",
+      };
+      res.redirect("/home");
+    } catch (error) {
+      console.error("Error undoing subscription cancellation:", error);
+      // On error, redirect to the home page
+      req.session.message = {
+        type: "error",
+        text: "Error undoing subscription cancellation, please refresh the page!",
+      };
+      res.redirect("/home");
+    }
+  });
 
-        if (customers.data.length > 0) {
-          const customerId = customers.data[0].id;
-          const subscriptions = await stripe.subscriptions.list({
-            customer: customerId,
+  router.post(
+      "/admin-cancel-subscription",
+      isAuthenticated,
+      async (req, res) => {
+        const { email } = req.body;
+
+        try {
+          const customers = await stripe.customers.list({
+            email: email,
+            limit: 1,
           });
 
-          for (const subscription of subscriptions.data) {
-            await stripe.subscriptions.cancel(subscription.id);
-          }
-        }
+          if (customers.data.length > 0) {
+            const customerId = customers.data[0].id;
+            const subscriptions = await stripe.subscriptions.list({
+              customer: customerId,
+            });
 
-        res.json({ message: "Cancellation was successful!" });
-      } catch (error) {
-        console.error("Error cancelling subscription:", error);
-        res.status(500).json({
-          message: "Error cancelling subscription. Please try again.",
-        });
+            for (const subscription of subscriptions.data) {
+              await stripe.subscriptions.cancel(subscription.id);
+            }
+          }
+
+          res.json({ message: "Cancellation was successful! The user will receive an email confirmation shortly" });
+        } catch (error) {
+          console.error("Error cancelling subscription:", error);
+          res.status(500).json({
+            message: "Error cancelling subscription. Please try again.",
+          });
+        }
       }
-    }
   );
 
   return router;
