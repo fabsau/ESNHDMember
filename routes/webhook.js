61ebe2a8e67580d814adee34d1c32597d7a0a768
fabio <github@sauna.re>
2023-09-04 14:30:32 +0200

    Added invoice attachment, templated mail for the invoice

--- routes/webhook.js
+++ routes/webhook.js
@@ -11,104 +11,137 @@ const pug = require("pug");
 module.exports = function (stripe) {
   router.use(bodyParser.raw({ type: "application/json" }));
 
-  // Send a test email when the application starts
+  // Test email sent on application startup
   mail.sendEmail(
     "Test email",
     "<p>This is a test email sent on application startup.</p>",
     "fabio@esn-heidelberg.de",
+    [],
   );
 
   router.post("/webhook", (request, response) => {
+    console.log("Webhook received");
     const event = request.body;
 
+    // Function to fetch PDF with retry mechanism
+    const fetchPdf = (url, path, retries = 3) => {
+      fetch(url)
+        .then((res) => res.buffer())
+        .then((buffer) => {
+          fs.writeFile(path, buffer, () => {
+            console.log("PDF downloaded and saved");
+
+            // After saving the PDF, send the email
+            stripe.subscriptions
+              .retrieve(subscriptionId)
+              .then((subscription) => {
+                console.log("Subscription retrieved");
+                const planName = subscription.plan.nickname;
+                console.log(`planName: ${planName}`);
+
+                // Create email content
+                const emailContent = pug.renderFile("../email/invoice.pug", {
+                  name: customerName,
+                  amount: formatCurrency(amountTotal, currency),
+                  planName: planName,
+                  invoiceId: invoiceId,
+                });
+
+                // Create email
+                const email = {
+                  to: customerEmail,
+                  from: process.env.GOOGLE_ADMIN_USER,
+                  subject: `Your invoice for ${planName}`,
+                  html: emailContent,
+                  attachments: [
+                    {
+                      filename: "invoice.pdf",
+                      path: invoicePdfPath,
+                      contentType: "application/pdf",
+                    },
+                  ],
+                };
+
+                // Send email
+                mail
+                  .sendEmail(email)
+                  .then(() => {
+                    console.log("Email sent");
+
+                    // After sending the email, delete the PDF
+                    fs.unlink(invoicePdfPath, (err) => {
+                      if (err) {
+                        console.error("Error deleting invoice PDF: ", err);
+                      } else {
+                        console.log("Invoice PDF deleted");
+                      }
+                    });
+                  })
+                  .catch((err) => {
+                    console.log("Error sending email: ", err);
+                  });
+              })
+              .catch((err) => {
+                console.log("Error retrieving subscription: ", err);
+              });
+          });
+        })
+        .catch((err) => {
+          if (retries > 0) {
+            console.log(
+              `Error downloading invoice PDF, retrying... ${retries} attempts left.`,
+            );
+            setTimeout(() => fetchPdf(url, path, retries - 1), 60000); // retry after 60 seconds
+          } else {
+            console.log("Error downloading invoice PDF: ", err);
+          }
+        });
+    };
+
     if (event.type === "checkout.session.completed") {
+      console.log("Handling checkout.session.completed event");
       const session = event.data.object;
 
       // Get customer ID from session
       const customerId = session.customer;
       const subscriptionId = session.subscription;
+      console.log(
+        `customerId: ${customerId}, subscriptionId: ${subscriptionId}`,
+      );
 
       // Retrieve customer from Stripe
       stripe.customers
         .retrieve(customerId)
         .then((customer) => {
+          console.log("Customer retrieved");
           const customerEmail = customer.email;
+          const customerName = customer.name;
+          console.log(
+            `customerEmail: ${customerEmail}, customerName: ${customerName}`,
+          );
 
           // Get additional details from the session
           const amountTotal = session.amount_total;
           const currency = session.currency;
           const paymentStatus = session.payment_status;
+          console.log(
+            `amountTotal: ${amountTotal}, currency: ${currency}, paymentStatus: ${paymentStatus}`,
+          );
 
           // Get invoice from Stripe
           stripe.invoices
             .retrieve(session.invoice)
             .then((invoice) => {
+              console.log("Invoice retrieved");
               const invoicePdfUrl = invoice.invoice_pdf;
+              const invoiceId = invoice.number;
               const invoicePdfPath = path.join(__dirname, "invoice.pdf");
+              console.log(
+                `invoicePdfUrl: ${invoicePdfUrl}, invoicePdfPath: ${invoicePdfPath}`,
+              );
 
               // Fetch the PDF and save it to a local file
-              fetch(invoicePdfUrl)
-                .then((res) => res.buffer())
-                .then((buffer) => {
-                  fs.writeFile(invoicePdfPath, buffer, () => {
-                    // Retrieve subscription from Stripe
-                    stripe.subscriptions
-                      .retrieve(subscriptionId)
-                      .then((subscription) => {
-                        // Create the email content
-                        const data = {
-                          customer: customer,
-                          subscription: subscription,
-                          invoice: invoice,
-                          amountTotal: amountTotal,
-                          currency: currency,
-                          paymentStatus: paymentStatus,
-                        };
-
-                        const emailContent = pug.renderFile(
-                          "../email/invoice.pug",
-                          data,
-                        );
-                        // Attach the PDF to the email
-                        mail
-                          .sendEmail(
-                            "Your Invoice",
-                            emailContent,
-                            customerEmail,
-                            null,
-                            null,
-                            null,
-                            null,
-                            [
-                              {
-                                path: invoicePdfPath,
-                                mimetype: "application/pdf",
-                              },
-                            ],
-                          )
-                          .then(() => {
-                            // Delete the PDF file after sending the email
-                            fs.unlink(invoicePdfPath, (err) => {
-                              if (err) {
-                                console.error(
-                                  `Error deleting file ${invoicePdfPath}: `,
-                                  err,
-                                );
-                              }
-                            });
-                          })
-                          .catch((err) => {
-                            console.log("Error sending email: ", err);
-                          });
-                      })
-                      .catch((err) => {
-                        console.log("Error retrieving subscription: ", err);
-                      });
-                  });
-                })
-                .catch((err) => {
-                  console.log("Error downloading invoice PDF: ", err);
-                });
+              fetchPdf(invoicePdfUrl, invoicePdfPath);
             })
             .catch((err) => {
               console.log("Error retrieving invoice: ", err);
